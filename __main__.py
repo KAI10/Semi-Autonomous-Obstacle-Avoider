65b3b5834539e977cd35012ecfc1d681b3596bb1
Kazi Ashik Islam <ikaziashik@gmail.com>
2016-12-14 15:22:01 +0600

    feedback codes added

--- __main__.py
+++ __main__.py
@@ -3,6 +3,7 @@
 import RTIMU
 import time
 import socket
+import numpy as np
 
 # necessary functions defined here
 from utilities import *
@@ -34,59 +35,85 @@ imu.setCompassEnable(True)
 ############ Robot Run Start From Here ####################
 
 def main():
-
     count = 0
     leftp.start(0)
     rightp.start(0)
+
+    firstReadTime = time.time()
+    yaw_values = list()
+    while time.time() - firstReadTime < 2:
+        acc_x, acc_y, yaw, yawrate = mpuRead(imu)
+        yaw_values.append(yaw)
+    ideal_yaw = np.mean(yaw_values)
 
     forward()
 
     # acc_x, acc_y, yaw, yawrate = mpuRead(imu)
     # adjusting at start
     # forwardAdjust(yaw, yawrate)
-   
+
+    past_yaw_error = 0.0
+    past_mpuReadTime = None
+
     try:
         while True:
+            measurement_start_time = time.time()
 
-            # Give Trigger pulse
-            GPIO.output(TRIG, 1)
-            time.sleep(.00001)
-            GPIO.output(TRIG, 0)
+            '''
+            sonar_distance = getSonarDistance()
+            '''
 
-            # Wait for device to send sound
-            while GPIO.input(ECHO) == 0:
-                pass
-
-            start = time.time()
-            # Wait for echo
-            while GPIO.input(ECHO) == 1:
-                pass
-            stop = time.time()
-
-            dist = (stop - start) * 17000;
-            print(dist)
-
+            mpuReadTime = time.time()
             acc_x, acc_y, yaw, yawrate = mpuRead(imu)
-            #print(acc_x, acc_y, yaw, yawrate)
-	    if False:
-            # if dist < min_obstacle_distance:
+            yaw_error = yaw - ideal_yaw
+
+            if past_mpuReadTime == None:
+                yaw_error_derivative = 0.0
+            else:
+                yaw_error_derivative = (yaw_error - past_yaw_error) / (mpuReadTime - past_mpuReadTime)
+
+            if False:
+                # if sonar_distance < min_obstacle_distance:
                 print("Obstacle Ahead!!!")
                 GPIO.output(LED, 1)
                 turnRight()
             else:
                 GPIO.output(LED, 0)
-                parameters.right_motor_dc, parameters.left_motor_dc, count = forwardAdjust(yaw, yawrate, parameters.right_motor_dc, parameters.left_motor_dc, count)
+                # parameters.right_motor_dc, parameters.left_motor_dc, count = forwardAdjust(yaw, yawrate, parameters.right_motor_dc, parameters.left_motor_dc, count)
+                forwardAdjust(yaw_error)
+                # forwardAdjustPD(yaw_error, yaw_error_derivative)
                 forward()
 
-            #time.sleep(measurement_interval)
+            past_yaw_error = yaw_error
+            past_mpuReadTime = mpuReadTime
 
     except KeyboardInterrupt:
         GPIO.cleanup()
         pass
 
-
 if __name__ == "__main__":
     main()
     GPIO.cleanup()
+
+
+def getSonarDistance():
+    # Give Trigger pulse
+    GPIO.output(TRIG, 1)
+    time.sleep(.00001)
+    GPIO.output(TRIG, 0)
+
+    # Wait for device to send sound
+    while GPIO.input(ECHO) == 0:
+        pass
+
+    sonar_echo_send_time = time.time()
+    # Wait for echo
+    while GPIO.input(ECHO) == 1:
+        pass
+    sonar_echo_recieve_time = time.time()
+
+    dist = (sonar_echo_recieve_time - sonar_echo_send_time) * 17000;
+    print(dist)
+    return dist
 
 ######################################################################
--- parameters.py
+++ parameters.py
@@ -11,6 +11,12 @@ def init():
     
     global yawRateThreshold
     global yawRateFactor
+
+    global yawErrorFactor
+    global yawErrorDerivativeFactor
+
+    global referenceQPose 
+    referenceQPose = (0.99705213, 0.0035882, -0.01229929, -0.07083318)
     
     min_obstacle_distance = 40
     
@@ -22,5 +28,11 @@ def init():
     
     measurement_interval = 0.015
     
+    yawDiffThreshold = 0.01745
+    yawDiffFactor = 5
+
     yawRateThreshold = 0.05
     yawRateFactor = 2
+
+    yawErrorFactor = 0.1
+    yawErrorDerivativeFactor = 0.001
